c48e66f58ab7a70963aea002a18e44de24ed170d
Denis Stepanov <denstepa@gmail.com>
2016-08-23 17:05:27 +0200

    ios part

---
+++ ios/RNUxcam/RNUXCam.h
@@ -0,0 +1,13 @@
+//
+//  RNUXCam.h
+//  RNUXCam
+//
+//  Created by Denis Stepanov on 23.08.16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import "RCTBridgeModule.h"
+
+@interface RNUXCam : NSObject <RCTBridgeModule>
+
+@end
---
+++ ios/RNUxcam/RNUXCam.m
@@ -0,0 +1,54 @@
+//
+//  RNUXCam.m
+//  RNUXCam
+//
+//  Created by Denis Stepanov on 23.08.16.
+//  Copyright © 2016 Facebook. All rights reserved.
+//
+
+#import "RNUXCam.h"
+#import <UXCam/UXCam.h> 
+
+@implementation RNUXCam
+
+RCT_EXPORT_METHOD(tagUsersName:(NSString*)userName:(RCTResponseSenderBlock)callback) {
+  if (userName.length>0)
+  {
+    [UXCam tagUsersName:userName additionalData:nil];
+    callback(@[[NSNumber numberWithBool:true]]);
+  }
+  else
+  {
+    callback(@[[NSNumber numberWithBool:false]]);
+  }
+}
+
+
+RCT_EXPORT_METHOD(tagScreenName:(NSString*)screenName:(RCTResponseSenderBlock)callback) {
+
+  if (screenName.length>0)
+  {
+    [UXCam tagScreenName:screenName];
+    
+    callback(@[[NSNumber numberWithBool:true]]);
+  }
+  else
+  {
+    callback(@[[NSNumber numberWithBool:false]]);
+  }
+}
+
+RCT_EXPORT_METHOD(addTag:(NSString*)tag:(RCTResponseSenderBlock)callback) {
+
+  if (tag.length>0)
+  {
+    [UXCam addTag:tag];
+    callback(@[[NSNumber numberWithBool:true]]);
+  }
+  else
+  {
+    callback(@[[NSNumber numberWithBool:false]]);
+  }
+}
+
+@end
